00ec1dd9284cef936eec1cb78efbe01f6de09c18
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-04 23:52:15 -0600

    initial devs

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const connectDV = async()=>{
+    try {
+        
+    } catch (error) {
+        console.log(`error: ${error.message}`)
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import chalk from "chalk";
 
 const app = express();
 
+app.use('/', (req,res)=>{
+    res.send('Hello Server')
+})
+
 app.listen(4005, ()=>{
     console.log(chalk.bgBlue(`Server is runnig on port `));
 })
