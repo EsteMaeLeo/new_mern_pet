edf3e9af5bc5d08f6008afce0e362be085e15d77
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-04 23:53:19 -0600

    initial devs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDV = async()=>{
+const connectDB = async()=>{
     try {
         
     } catch (error) {
         console.log(`error: ${error.message}`)
+        process.exit(1)
     }
 }
+
+export default connectDB
