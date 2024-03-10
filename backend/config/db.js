74bc7fc6040c81544dae731149c42a5ea52a11a8
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-09 22:02:06 -0600

    initial devs

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async()=>{
             "mongodb+srv://",
 )
         const url = `${db.connection.host}:${db.connection.port}`
-        console.log(`Mongo Connectedto ${url}`)
+        console.log(`Mongo Connected to ${url}`)
     } catch (error) {
         console.log(`error: ${error.message}`)
         process.exit(1)
