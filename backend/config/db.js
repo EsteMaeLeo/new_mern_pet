b2498dd6ace53cca8f8eaf1600fe950e4d793d8e
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-10 21:42:40 -0500

    initial devs

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
     try {
         const db = await mongoose.connect(
             //process.env.MMONGO_URI,
-            "mongodb+srv://@cluster0.bayy0.mongodb.net/?retryWrites=true&w=majority&appName=petshop",
+            process.env.MONGO_URI,
 )
         const url = `${db.connection.host}:${db.connection.port}`
         console.log(`Mongo Connected to ${url}`)
