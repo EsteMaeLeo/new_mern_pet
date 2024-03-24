0cabf40ad9d34b5c97cf9b56bc9e5f815aee690a
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-23 22:32:51 -0500

    new changes

--- backend/models/veterinarian.js
+++ backend/models/veterinarian.js
@@ -1,9 +1,39 @@
 import mongoose from "mongoose";
 
-const veterinarian = mongoose.Schema({
+const veterinarianShema = mongoose.Schema({
   name: {
     type: String,
     require: true,
     trim: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    default: null,
+    trim: true,
+  },
+  web: {
+    type: String,
+    default: null,
+  },
+  token: {
+    type: String,
+  },
+  confirm: {
+    type: String,
+    default: false,
+  },
 });
+
+const veterinarian = mongoose.model("veterinarian", veterinarianShema);
+
+export default veterinarian;
