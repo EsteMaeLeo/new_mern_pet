cd3b694b35e6b16ed551741e32902f0e5ede2758
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-26 23:40:48 -0500

    adding new changes

---
+++ backend/controllers/vetController.js
@@ -0,0 +1,9 @@
+const register = (req, res) => {
+  res.send("FROM API VETS register DUDE!!!!");
+};
+
+const profile = (req, res) => {
+  res.send("FROM API API/VET/profile");
+};
+
+export { register, profile };
--- backend/routes/vetRoutes.js
+++ backend/routes/vetRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 
+import { register, profile } from "../controllers/vetController.js";
+
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get("/register", register);
 
-})
+router.get("/profile", profile);
 export default router;
