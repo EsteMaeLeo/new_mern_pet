7e97bf14db41f802f92f90925fa0abddc996bdf4
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-04-05 00:20:43 -0500

    new changes

--- backend/controllers/vetController.js
+++ backend/controllers/vetController.js
@@ -1,14 +1,24 @@
-const register = (req, res) => {
-  console.log(req.body)
-  const {email, password, name} = req.body;
-  console.log(name)
-  console.log(email)
-  console.log(password)
-  res.json({msg:"Creating the user"});
+import veterinarian from "../models/veterinarian.js";
+
+const register = async (req, res) => {
+  console.log(req.body);
+  const { email, password, name } = req.body;
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
+  try {
+    //save new veterinarian
+    const vet = new veterinarian(req.body);
+    const vetSave = await vet.save();
+    res.json({ msg: "Creating the user" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const profile = (req, res) => {
-    res.json({msg:"USER info"});
+  res.json({ msg: "USER info" });
 };
 
 export { register, profile };
