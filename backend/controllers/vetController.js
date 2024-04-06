c758fcef3829573f7edf29251e4d965021c27b63
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-04-05 19:39:51 -0500

    new changes

--- backend/controllers/vetController.js
+++ backend/controllers/vetController.js
@@ -3,6 +3,14 @@ import veterinarian from "../models/veterinarian.js";
 const register = async (req, res) => {
   console.log(req.body);
   const { email, password, name } = req.body;
+
+  const findUser = await veterinarian.findOne({ email });
+
+  if (findUser) {
+    const error = new Error("User with the same email already register");
+    return res.status(400).json({ msg: error.message });
+  }
+
   console.log(name);
   console.log(email);
   console.log(password);
@@ -11,7 +19,7 @@ const register = async (req, res) => {
     //save new veterinarian
     const vet = new veterinarian(req.body);
     const vetSave = await vet.save();
-    res.json({ msg: "Creating the user" });
+    res.json(vetSave);
   } catch (error) {
     console.log(error);
   }
