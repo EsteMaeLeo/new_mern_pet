83d32582fc49d258ea5260fc3d2587818bec5949
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-04-23 23:58:29 -0500

    adding new changes

--- backend/middleware/outMiddleware.js
+++ backend/middleware/outMiddleware.js
@@ -3,6 +3,16 @@ const checkAuth = (req, res, next) => {
   console.log(req.headers);
   console.log(req.header.authorization);
 
+  if (
+    req.header.authorization &&
+    req.header.authorization.startsWith("Bearer")
+  ) {
+    console.log("Has Token Valid  with Bearer");
+  } else {
+    const error = new Error("Token not valid or doesnt exist.....");
+    return res.status(403).json({ msg: error.message });
+  }
+
   next();
 };
 
