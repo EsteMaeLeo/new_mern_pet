83fbab2c2d3e43dd1ad90f62247158459b19fbb2
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-04-26 23:13:29 -0500

    adding new changes

--- backend/middleware/outMiddleware.js
+++ backend/middleware/outMiddleware.js
@@ -1,17 +1,30 @@
+import jwt from "jsonwebtoken";
+import veterinarian from "../models/veterinarian.js";
+
 const checkAuth = (req, res, next) => {
   console.log("from middleware");
   console.log(req.headers);
   console.log(req.headers.authorization);
+  let token;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     console.log("Has Token Valid  with Bearer");
-  } else {
-    const error = new Error("Token not valid or doesnt exist.....");
-    return res.status(403).json({ msg: error.message });
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decoded)
+      const veterinarian
+    } catch (error) {
+      const e = new Error("Token not valid...");
+      res.status(403).json({ msg: e.message });
+    }
   }
+
+  const error = new Error("Token not valid or doesnt exist.....");
+  res.status(403).json({ msg: error.message });
 
   next();
 };
