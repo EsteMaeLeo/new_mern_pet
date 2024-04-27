977d3d9b6c6fb38b324ed4422a4e966e3fa56005
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-04-26 23:40:59 -0500

    adding new changes

--- backend/middleware/outMiddleware.js
+++ backend/middleware/outMiddleware.js
@@ -1,30 +1,33 @@
 import jwt from "jsonwebtoken";
-import veterinarian from "../models/veterinarian.js";
+import Veterinarian from "../models/veterinarian.js";
 
-const checkAuth = (req, res, next) => {
-  console.log("from middleware");
-  console.log(req.headers);
-  console.log(req.headers.authorization);
+const checkAuth = async (req, res, next) => {
   let token;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    console.log("Has Token Valid  with Bearer");
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded)
-      const veterinarian
+      console.log(decoded);
+      const vet = await Veterinarian.findById(decoded.id).select(
+        "-password -token -confirm"
+      );
+      req.veterinario = vet;
+      console.log(vet);
+      return next();
     } catch (error) {
       const e = new Error("Token not valid...");
-      res.status(403).json({ msg: e.message });
+      return res.status(403).json({ msg: e.message });
     }
   }
 
-  const error = new Error("Token not valid or doesnt exist.....");
-  res.status(403).json({ msg: error.message });
+  if (!token) {
+    const error = new Error("Token not valid or doesnt exist.....");
+    res.status(403).json({ msg: error.message });
+  }
 
   next();
 };
