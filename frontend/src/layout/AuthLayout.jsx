1b77d893fa2df1044d7fd8c902616bd35b24fdb9
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-17 23:13:03 -0500

    new changes

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      <h1>Vet and patients Manager</h1>
       <main className="container mx-auto grid grid-cols-2">
         <Outlet />
       </main>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 const Login = () => {
   return (
     <>
-      
+      <div>
+        <h1 className="text-indigo-600">Login and Manage your patients</h1>
+      </div>
     </>
   );
 };
