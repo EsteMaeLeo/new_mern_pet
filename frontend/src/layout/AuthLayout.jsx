8308ca962f6e42738094b32b438f8c78a39d38cb
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-17 23:07:43 -0500

    new changes

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      <h1>From Auth Layout</h1>
-      <Outlet />
+      <h1>Vet and patients Manager</h1>
+      <main className="container mx-auto grid grid-cols-2">
+        <Outlet />
+      </main>
     </>
   );
 };
