a37d8447a0f9a484f740ecc21cbc3cb54f2230a4
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-18 23:00:10 -0500

    new changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,39 @@ const Login = () => {
   return (
     <>
       <div>
-        <h1 className="text-indigo-600">Login and Manage your patients</h1>
+        <h1 className="text-indigo-600 font-black text-5xl">
+          Login and Manage your <span className="text-black">Patients</span>
+        </h1>
+      </div>
+      <div>
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Your password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Login"
+            className="bg-indigo-600 w-full py-3 px-25 rounded-2xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800"
+          />
+        </form>
       </div>
     </>
   );
