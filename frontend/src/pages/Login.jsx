0cf5e08aeb89d518b95004bf7256e5b9774af06b
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-21 23:44:32 -0500

    new changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
           Login and Manage your <span className="text-black">Patients</span>
         </h1>
       </div>
-      <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-xl">
+      <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-xl bg-white">
         <form>
           <div className="my-5">
             <label className="uppercase text-gray-7-- block text-xl font-bold">
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,6 +6,49 @@ export const Signup = () => {
           SignUp and Manage your <span className="text-black">Patients</span>
         </h1>
       </div>
+
+      <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-lg bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Name
+            </label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Your Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Login"
+            className="bg-indigo-600 w-full py-3 px-25 rounded-2xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800"
+          />
+        </form>
+      </div>
     </>
   );
 };
