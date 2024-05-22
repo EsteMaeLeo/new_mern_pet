926814c4f0b8acd8d3b3249c9d176e133b75e35c
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-21 23:48:13 -0500

    new changes

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,6 @@
-export const Signup = () => {
+import { Link } from "react-router-dom";
+
+const Signup = () => {
   return (
     <>
       <div>
@@ -42,12 +44,35 @@ export const Signup = () => {
             />
           </div>
 
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              placeholder="Your Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
           <input
             type="submit"
             value="Login"
             className="bg-indigo-600 w-full py-3 px-25 rounded-2xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800"
           />
         </form>
+
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link className="block text-center my-5 text-gray-600" to="/">
+            Do you have account? Sign
+          </Link>
+          <Link
+            className="block text-center my-5 text-gray-600"
+            to="/forgot-password"
+          >
+            Forgot Password
+          </Link>
+        </nav>
       </div>
     </>
   );
