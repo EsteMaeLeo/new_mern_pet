32c148e87b96bae06b6f038e2b7e7ca026543c67
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-29 23:54:29 -0500

    new changes

--- backend/helpers/emailSignup.js
+++ backend/helpers/emailSignup.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const emailSignup = (data) =>{
+const emailSignup = async (data) =>{
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -9,7 +9,18 @@ const emailSignup = (data) =>{
           pass: process.env.EMAIL_PASS
         }
       });
-      console.log(data)
+      
+      //sent email
+      const {email, name, token} = data;
+
+      const info = await WebTransportError.sendMail({
+        from: 'VET App!',
+        to:email,
+        subject: 'Check your account on VET App!',
+        text: 'Check your account on VET App!',
+        html: `
+        `
+      })
 }
 
 export default emailSignup;
