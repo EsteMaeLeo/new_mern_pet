476d46cdc20d9400751056afe83fe38fc83291c9
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-30 00:20:55 -0500

    new changes

--- backend/helpers/emailSignup.js
+++ backend/helpers/emailSignup.js
@@ -1,28 +1,32 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-const emailSignup = async (data) =>{
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-      });
-      
-      //sent email
-      const {email, name, token} = data;
+const emailSignup = async (data) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-      const info = await WebTransportError.sendMail({
-        from: 'VET App!',
-        to:email,
-        subject: 'Check your account on VET App!',
-        text: 'Check your account on VET App!',
-        html: `<p> Hello ${name} check you account on VET App!</p>
+  //sent email
+  const { email, name, token } = data;
+
+  const info = await WebTransportError.sendMail({
+    from: "VET App!",
+    to: email,
+    subject: "Check your account on VET App!",
+    text: "Check your account on VET App!",
+    html: `<p> Hello ${name} check you account on VET App!</p>
                <p> Your account is allready created check on the link: 
-            <a href="">Check account</a></p>
-        `
-      })
-}
+               <a href="${process.env.FRONTEND_URL}/confirm/${token}">Check account</a></p>
+
+               <p>Ignore the message if you didnt create the account</p>
+        `,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+};
 
 export default emailSignup;
