dffe37b077cc52eb2983d6a2a73ea196efd111a9
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-05-30 22:52:55 -0500

    new changes

--- frontend/src/pages/ConfimUser.jsx
+++ frontend/src/pages/ConfimUser.jsx
@@ -1,9 +1,14 @@
 const ConfimUser = () => {
-    return (
-        <>
-          <h1>From ConfirUser</h1>
-        </>
-      );
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-5xl">
+          Confirm your account and Manage your <span className="text-black">Patients</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-lg bg-white"></div>
+    </>
+  );
 };
 
 export default ConfimUser;
