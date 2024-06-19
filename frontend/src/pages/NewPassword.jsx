d882784347c2d17c88349009dfc50614657dc937
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-06-18 23:51:39 -0500

    new changes

--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -3,8 +3,25 @@ const NewPassword = () => {
     <>
       <div>
         <h1 className="text-indigo-600 font-black text-5xl">
-          Recover your password and keep record of <span className="text-black">Your Patients</span>
+          Recover your password and keep record of{" "}
+          <span className="text-black">Your Patients</span>
         </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-lg bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-7-- block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Your Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </form>
       </div>
     </>
   );
