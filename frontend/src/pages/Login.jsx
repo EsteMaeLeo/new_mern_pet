5e7e1dfbf367c0d5f7006526ee1bb6fc0863204b
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-07-29 23:57:35 -0500

    new changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 const Login = () => {
+
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('')
+
   return (
     <>
       <div>
