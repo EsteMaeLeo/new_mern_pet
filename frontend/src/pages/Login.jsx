f807d28ebdffcb13be14e345e6e2fc24165059ee
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-08-03 22:01:47 -0500

    new changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,16 +4,23 @@ import useAuth from "../hooks/useAuth";
 import Alert from "../components/Alert";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState({});
 
-  const[email, setEmail] = useState('');
-  const[password, setPassword] = useState('')
-  const[alert, setAlert] = useState({})
+  const { msg } = alert;
 
-  const {msg} = alert
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-  }
+    if ([email, password].includes("")) {
+      setAlert({
+        msg: "All fields are mandatory",
+        error: true,
+      });
+      return;
+    }
+  };
 
   return (
     <>
@@ -23,8 +30,7 @@ const Login = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-xl px-5 py-10 rounded-xl bg-white">
-
-      {msg && <Alert alert={{alert}}/>}
+        {msg && <Alert alert={{ alert }} />}
 
         <form onSubmit={handleSubmit}>
           <div className="my-5">
@@ -36,7 +42,7 @@ const Login = () => {
               placeholder="Your Email"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
               value={email}
-              onChange={e=> setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -48,7 +54,7 @@ const Login = () => {
               placeholder="Your password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
               value={password}
-              onChange={e=> setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
