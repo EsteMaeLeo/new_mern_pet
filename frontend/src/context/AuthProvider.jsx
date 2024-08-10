dde39f907fa742cbffec33f649fa0613141cc38b
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-08-09 23:41:27 -0500

    new changes

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -2,21 +2,30 @@ import { useState, useEffect, createContext } from "react";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children} ) => {
+const AuthProvider = ({ children }) => {
+  //const {children} = props;
+  const [auth, setAuth] = useState({});
 
-    //const {children} = props;
-    const [auth, setAuth] = useState({})
-    console.log(children)
-    return(
-        <AuthContext.Provider>
-            value={{
-                auth,
-                setAuth
-            }}
-        </AuthContext.Provider>
-    )
+  useEffect(() => {
+    const authUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) return;
+      console.log(token);
+    };
+    authUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider>
+      value=
+      {{
+        auth,
+        setAuth,
+      }}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthProvider };
 export default AuthContext;
-
