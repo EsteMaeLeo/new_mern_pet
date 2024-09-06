de30f819ecc2129fe5ec34519fc34e5cad1cb146
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-05 23:47:30 -0500

    new changes

--- frontend/src/layout/ProtectedRoute.jsx
+++ frontend/src/layout/ProtectedRoute.jsx
@@ -1,16 +1,19 @@
 import { Outlet, Navigate } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import useAuth from "../hooks/useAuth";
 
 const ProtectedRoute = () => {
   const { auth, load } = useAuth();
   console.log(auth);
   console.log(load);
-  console.log(auth.profile?._id)
+  console.log(auth.profile?._id);
   if (load) return "loading...";
   return (
     <>
-      <h1>This ADMIN</h1>
-      {auth.profile?._id ? <Outlet /> : <Navigate to="/" />}
+      <Header />
+        {auth.profile?._id ? <Outlet /> : <Navigate to="/" />}
+      <Footer />
     </>
   );
 };
