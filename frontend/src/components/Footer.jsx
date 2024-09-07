ef5b7f26e601e8b61dba38b44d4dabf500c3cfc6
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-06 23:28:11 -0500

    new changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,11 @@
-
-
 function Footer() {
-    return (
-      <h1>Footer</h1>
-    )
-  }
-  
-  export default Footer
-  
+  return( 
+  <footer className="py-10">
+    <p className="text-center font-bold">VET App {''} 
+        <span className="text-indigo-600"> Patients Management</span>
+    </p>
+  </footer>
+  )
+}
+
+export default Footer;
