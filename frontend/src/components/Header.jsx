1c608c81f1d746b2af5dba8c1fa97ed40c69e262
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-12 22:01:06 -0500

    new changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,14 @@
 function Header() {
-  return <header></header>;
+  return (
+    <header className="py-10 bg-indigo-600">
+      <div className="container mx-auto">
+        <h1>
+          Vet Patient {""}
+          <span className="text-white">Management </span>
+        </h1>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
