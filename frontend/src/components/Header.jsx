05b8b9a38be372944fa7395fbbe1ca210b5ab71c
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-13 22:34:24 -0500

    new changeS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
   return (
     <header className="py-10 bg-indigo-600">
       <div className="container mx-auto">
-        <h1>
+        <h1 className="font-bold text-2xl text-indigo-200">
           Vet Patient {""}
-          <span className="text-white">Management </span>
+          <span className="text-white font-black">Management </span>
         </h1>
       </div>
     </header>
