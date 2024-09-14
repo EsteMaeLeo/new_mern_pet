fd8e715e19cb9a679e7680b2f4bd8a709a9c05f7
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-13 22:39:08 -0500

    new changeS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header className="py-10 bg-indigo-600">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between">
         <h1 className="font-bold text-2xl text-indigo-200">
           Vet Patient {""}
           <span className="text-white font-black">Management </span>
         </h1>
+        <nav className="flex gap-4">
+          <Link to="/admin" className="text-white text-xl">
+            Patients
+          </Link>
+          <Link to="/admin" className="text-white text-xl">
+            Profile
+          </Link>
+        </nav>
       </div>
     </header>
   );
