07fcdd84059c379a189b1ee40fc12d802a063f40
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-09-19 22:05:31 -0500

    new changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="py-10 bg-indigo-600">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex flex-col justify-between items-center">
         <h1 className="font-bold text-2xl text-indigo-200">
           Vet Patient {""}
           <span className="text-white font-black">Management </span>
