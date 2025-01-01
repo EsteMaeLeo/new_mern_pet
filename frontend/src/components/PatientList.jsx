98db7cd07ede3d53b2e17d512f8a3d22c31ef14a
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-12-31 18:57:37 -0600

    changes

---
+++ frontend/src/components/PatientList.jsx
@@ -0,0 +1,9 @@
+const PatientList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PatientList
--- frontend/src/pages/AdminPatient.jsx
+++ frontend/src/pages/AdminPatient.jsx
@@ -1,9 +1,18 @@
 import { Outlet } from "react-router-dom";
+import Form from "../components/Form";
+import PatientList from "../components/PatientList";
 
 const AdminPatient = () => {
   return (
     <>
-      <div></div>
+      <div className="flex">
+        <div className="md:w-1/2">
+          <Form />
+        </div>
+        <div className="md:w-1/2">
+          <PatientList />
+        </div>
+      </div>
       <Outlet />
     </>
   );
