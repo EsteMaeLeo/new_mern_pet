192adaa8204a66c975bebb2a5d35836ab723af1a
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-06 23:57:04 -0600

    new changes

--- frontend/src/pages/AdminPatient.jsx
+++ frontend/src/pages/AdminPatient.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Form from "../components/Form";
 import PatientList from "../components/PatientList";
+import { useState } from "react";
 
 const AdminPatient = () => {
+  
+  const [showForms, setShowForm] = useState(false)
+  
   return (
     <>
-      <div className="flex">
-        <div className="md:w-1/2">
+      <div className="flex flex-col md:flex-row">
+        <div className={`${showForms ? 'block' : 'hidden'}md:w-1/2`}>
           <Form />
         </div>
         <div className="md:w-1/2">
