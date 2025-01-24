885b4e972c6972d40fc6ce12bab3618b2943510a
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-23 23:56:26 -0600

    changes

---
+++ frontend/src/context/PatientProvider.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState, useEffect } from "react";
+import clientAxios from "../config/axios";
+
+const PatientContext = createContext()
+
+export const PatientProvider = () => {
+    return
+        <PatientContext.Provider>
+            
+        </PatientContext.Provider>
+}
