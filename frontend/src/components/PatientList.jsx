c145d12aa265c00adbedf24f226111be2426d8b7
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-29 00:10:41 -0600

    changes

--- frontend/src/components/PatientList.jsx
+++ frontend/src/components/PatientList.jsx
@@ -1,27 +1,34 @@
-import usePatient from "../hooks/usePatient"
+import usePatient from "../hooks/usePatient";
 
 const PatientList = () => {
-
-  const {patients} = usePatient()
+  const { patients } = usePatient();
 
   return (
     <>
-      {patients.length ? 'Patients List':
-    (
-      <>
-        <h2 className="font-black text-3xl text-center">No Patients Found</h2>
-        <p className="text-xl mt-5 mb-10 text-center">
-          Add patients first! {''}
-          <span className="text-indigo-600 font-bold">
-            and will show in this page
-          </span>
-        </p>
-      </>
-    )  
-    
-    }
-    </>
-  )
-}
+      {patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">List of Patients</h2>
 
-export default PatientList
+          <p className="text-xl mt-5 mb-10 text-center">
+            Manage your{""}
+            <span className="text-indigo-600 font-bold">
+             Patients and Appoitments
+            </span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No Patients Found</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Add patients first! {""}
+            <span className="text-indigo-600 font-bold">
+              and will show in this page
+            </span>
+          </p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default PatientList;
