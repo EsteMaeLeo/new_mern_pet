812e427d0d3842b2d48bcaf4370d1d18b5e7194b
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-29 00:08:14 -0600

    changes

--- frontend/src/components/PatientList.jsx
+++ frontend/src/components/PatientList.jsx
@@ -6,7 +6,20 @@ const PatientList = () => {
 
   return (
     <>
-      {patients.length }
+      {patients.length ? 'Patients List':
+    (
+      <>
+        <h2 className="font-black text-3xl text-center">No Patients Found</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+          Add patients first! {''}
+          <span className="text-indigo-600 font-bold">
+            and will show in this page
+          </span>
+        </p>
+      </>
+    )  
+    
+    }
     </>
   )
 }
