0acf417a2ea0a9ec75ffb7072e8a98f623cee021
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-31 23:56:43 -0600

    changes

--- frontend/src/components/Patient.jsx
+++ frontend/src/components/Patient.jsx
@@ -1,5 +1,10 @@
-const Patient = () => {
-  return <h1>Patient</h1>;
+const Patient = ({patient}) => {
+    const {email, registerDate, name, owner, symptoms, _id} = patient
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md px-0 py-10 rounded-xl">
+
+    </div>
+  );
 };
 
 export default Patient;
