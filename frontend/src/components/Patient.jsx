619cb70f7784bb8bf8ebf130917328a00cffd617
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-01-31 23:57:41 -0600

    changes

--- frontend/src/components/Patient.jsx
+++ frontend/src/components/Patient.jsx
@@ -2,7 +2,9 @@ const Patient = ({patient}) => {
     const {email, registerDate, name, owner, symptoms, _id} = patient
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-0 py-10 rounded-xl">
-
+        <p className="font-bold uppercase">Name:
+            <span>{name}</span>
+        </p>
     </div>
   );
 };
