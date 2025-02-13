13d5c666655bf9dd23cd8b440d3d84bfd67dfad6
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-02-12 23:58:16 -0600

    add new changes

--- frontend/src/context/PatientProvider.jsx
+++ frontend/src/context/PatientProvider.jsx
@@ -29,20 +29,31 @@ export const PatientProvider = ({ children }) => {
   }, []);
 
   const savePatient = async (patient) => {
-    try {
-      const token = localStorage.getItem("token");
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await clientAxios.post("/patients", patient, config);
 
-      const { __V, ...patienSave } = data;
-      setPatients([patienSave, ...patients]);
-    } catch (error) {
-      console.log(error.response.data.msg);
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    if (patient.id) {
+      try {
+        const {data} = await clientAxios.put(`/patient/${patient._id}`, patient, config)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      try {
+
+        const { data } = await clientAxios.post("/patient", patient, config);
+
+        const { __V, ...patienSave } = data;
+        setPatients([patienSave, ...patients]);
+      } catch (error) {
+        console.log(error.response.data.msg);
+      }
     }
   };
 
