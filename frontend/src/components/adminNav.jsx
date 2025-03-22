6bd94229a47f9e512698af9985105f07bf9e22f3
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-03-21 23:48:05 -0500

    new changes

--- frontend/src/components/adminNav.jsx
+++ frontend/src/components/adminNav.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 function adminNav() {
   return (
-    <nav>
+    <nav className="flex gap-3">
       <link to="/admin/profile" className="font-bold uppercase text-gray-500">
         Profile
       </link>
-      <link to="/admin/change-password" className="font-bold uppercase text-gray-500">
+      <link
+        to="/admin/change-password"
+        className="font-bold uppercase text-gray-500"
+      >
         Change Password
       </link>
     </nav>
--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,11 +1,11 @@
-
-
+import adminNav from "../components/adminNav";
 const ChangePassword = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <adminNav />
+      <h2 className="font-black text-3xl text-center mt-10">Edit Profile</h2>
+    </>
+  );
+};
 
 export default ChangePassword;
