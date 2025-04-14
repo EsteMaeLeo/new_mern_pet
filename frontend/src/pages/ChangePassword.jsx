a6051d216d30fcd242b799d5268b7231d3d130a6
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-04-13 23:37:01 -0500

    new changes

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -4,9 +4,9 @@ const ChangePassword = () => {
   return (
     <>
       <adminNav />
-      <h2>Change Password</h2>
-      <p>
-        Modify your {""} <span>Password here</span>
+      <h2 className="font-black text-3xl text-center mt-10">Change Password</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Modify your {""} <span className="text-indigo-600 font-bold">Password here</span>
       </p>
     </>
   );
