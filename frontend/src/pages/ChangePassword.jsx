eb78cd6995f28c844b39e8057ff01c9ea56e53f9
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-04-13 23:35:20 -0500

    new changes

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,9 +1,13 @@
 import adminNav from "../components/adminNav";
+
 const ChangePassword = () => {
   return (
     <>
       <adminNav />
-      
+      <h2>Change Password</h2>
+      <p>
+        Modify your {""} <span>Password here</span>
+      </p>
     </>
   );
 };
