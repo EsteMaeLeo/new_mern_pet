26bbb7dff533d1671b662b3032e6e7f35c5021e3
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-04-14 23:39:32 -0500

    new changes

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -6,7 +6,8 @@ const ChangePassword = () => {
       <adminNav />
       <h2 className="font-black text-3xl text-center mt-10">Change Password</h2>
       <p className="text-xl mt-5 mb-10 text-center">
-        Modify your {""} <span className="text-indigo-600 font-bold">Password here</span>
+        Modify your {""}{" "}
+        <span className="text-indigo-600 font-bold">Password here</span>
       </p>
     </>
   );
--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -3,6 +3,11 @@ function EditProfile() {
   return (
     <>
       <adminNav />
+      <h2 className="font-black text-3xl text-center mt-10">Edit your profile</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Modify your {""}{" "}
+        <span className="text-indigo-600 font-bold">Information here</span>
+      </p>
     </>
   );
 }
