dd8ddfc8eb9450e5f837a7b938652e9fac763f91
EsteMaeLeo <leonel_morales20@hotmail.com>
2025-04-17 23:16:19 -0500

    new changes

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -3,7 +3,9 @@ function EditProfile() {
   return (
     <>
       <adminNav />
-      <h2 className="font-black text-3xl text-center mt-10">Edit your profile</h2>
+      <h2 className="font-black text-3xl text-center mt-10">
+        Edit your profile
+      </h2>
       <p className="text-xl mt-5 mb-10 text-center">
         Modify your {""}{" "}
         <span className="text-indigo-600 font-bold">Information here</span>
@@ -11,7 +13,16 @@ function EditProfile() {
 
       <div className="flex justify-center">
         <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
-          <form action=""></form>
+          <form>
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Name</label>
+              <input
+                type="text"
+                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                name="name"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </>
